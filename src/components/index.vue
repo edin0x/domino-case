f877e17ef005f7026548fdd51a4ce77825244946
Edin Dudojević <edudojevic@users.noreply.github.com>
2017-12-17 13:12:50 +0100

    Add styling Index page

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,22 +1,23 @@
 <template>
-    <div>
-      <h1>This is the visualization you always wanted</h1>
-      You came at the right place. This place will amaze you with awesome visualizations of US states,
-      and we promise to deliver even more in the near future!
+  <div class="ui center aligned very padded text basic segment">
+    <i class="huge pie chart icon"></i>
+    <h1>This is the visualization you always wanted</h1>
+    <p></p>
 
-      <h2>Choose one of the below visualizations:</h2>
-      <ul>
-        <li><router-link to="/usa">US states</router-link></li>
-      </ul>
+    <h2>Choose one of the below visualizations:</h2>
+    <div class="ui centered cards">
+      <div class="card">
+        <div class="content">
+          <div class="header">United states</div>
+          <div class="meta">Population and jobs</div>
+          <div class="description">Visualization of population composition and job type distribution.</div>
+          <div class="extra content">
+            <div class="ui center aligned basic segment">
+              <router-link to="/usa"><div class="ui fluid primary button">View</div></router-link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
-
-<script>
-    export default {
-        name: "index"
-    }
-</script>
-
-<style scoped>
-
-</style>
