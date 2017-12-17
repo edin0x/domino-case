51692fb32bdf7db572f172f0fa6673bfecf6bb93
Edin Dudojević <edudojevic@users.noreply.github.com>
2017-12-17 15:31:08 +0100

    Setup usa service and load data by using it

---
+++ src/services/usa-service.js
@@ -0,0 +1,15 @@
+class CountryService {
+  getJobTypes() {
+    return fetch('/static/data/usa/jobs.json').then(response => response.json())
+  }
+
+  getPopulationComposition() {
+    return fetch('/static/data/usa/population.csv')
+  }
+
+  getStateData() {
+    return fetch('/static/data/usa/states.json').then(response => response.json())
+  }
+}
+
+export default new CountryService();
