41475e16ddacae727429add8d2f2aaf070be659d
Edin Dudojević <edudojevic@users.noreply.github.com>
2017-12-17 13:12:01 +0100

    Add Semantic UI styling

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <template>
-  <div id="app">
+  <div id="app" class="ui container">
     <router-view/>
   </div>
 </template>
 
 <script>
+import 'semantic-ui-css/semantic.css';
+
 export default {
   name: 'app',
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
