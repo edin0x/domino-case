26c75d5cc898b4cc83053a2ce175bfa447fc8f77
Edin Dudojević <edudojevic@users.noreply.github.com>
2017-12-17 22:42:44 +0100

    Add Bar Chart component

---
+++ src/components/visualizations/bar-chart.vue
@@ -0,0 +1,42 @@
+<script>
+  import { Bar } from 'vue-chartjs';
+
+  export default {
+    extends: Bar,
+    name: "bar-chart",
+    props: ['chartData'],
+    mounted() {
+      this.renderChart({
+        labels: this.chartData.map(d => d.label),
+        datasets: [
+          {
+            backgroundColor: 'steelblue',
+            data: this.chartData.map(d => d.value),
+          },
+        ],
+        scales: {
+          xAxes: [ {
+            categoryPercentage: 2.9,
+            barPercentage: 20
+          }]
+        }
+      }, {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            barPercentage: 1.5
+          }]
+        },
+        legend: {
+          display: false,
+        }
+      });
+    },
+    watch: {
+      chartData() {
+
+      }
+    }
+  }
+</script>
